eda26517f4da0a70d446057aa432b22437f19b67
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2023-11-10 16:02:38 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Is beef current ill?" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <span style="color: red;">Yes</span> beefy baeith is currently lay up in bed, ill as a bofa can be
         </p>
       </main>
 
